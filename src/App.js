e69c911847673a5f24915fb5b3dba8141afbd5ea
Robin Burrage <robin.o.burrage@gmail.com>
2021-08-23 15:05:25 -0400

    Updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-  <div>
-  <Header />
-  <Switch>
-    <Route exact path='/' component={HomePage} />  
-    <Route path='/shop' component={ShopPage} />  
-  </Switch>
-  </div>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
   )
 }
 
